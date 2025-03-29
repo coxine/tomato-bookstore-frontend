1ad867aeee317944a7d48216dfda487bb61249b9
栖鸟闲榆 <59347549+coxine@users.noreply.github.com>
2025-03-29 11:55:07 +0800

    feat: add InfoCard component for displaying information with actions

---
+++ src/components/UI/InfoCard.tsx
@@ -0,0 +1,37 @@
+import { Box, Card, CardActions, CardOverflow, Divider, Stack, Typography } from '@mui/joy';
+
+export interface InfoCardProps {
+  title: string
+  description?: string
+  children: React.ReactNode
+  actions?: React.ReactNode
+}
+
+export default function InfoCard(
+  { title, description, children, actions }: InfoCardProps
+) {
+  return <Stack
+    spacing={4}
+    sx={{
+      display: 'flex',
+      maxWidth: '800px',
+      mx: 'auto',
+      px: { xs: 2, md: 6 },
+      py: { xs: 2, md: 3 },
+    }}
+  >
+    <Card>
+      <Box>
+        <Typography level="title-md">{title}</Typography>
+        <Typography level="body-sm">{description}</Typography>
+      </Box>
+      <Divider />
+      {children}
+      <CardOverflow sx={{ borderTop: '1px solid', borderColor: 'divider' }}>
+        <CardActions sx={{ alignSelf: 'flex-end', pt: 2 }}>
+          {actions}
+        </CardActions>
+      </CardOverflow>
+    </Card>
+  </Stack>;
+}
