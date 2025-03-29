34341b7957c6fbc7e27a490a7a0164d3ad6af7ea
栖鸟闲榆 <59347549+coxine@users.noreply.github.com>
2025-03-29 12:04:35 +0800

    feat: add Sidebar component

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,157 @@
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'; import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
+import Box from '@mui/joy/Box';
+import GlobalStyles from '@mui/joy/GlobalStyles';
+import Input from '@mui/joy/Input';
+import List from '@mui/joy/List';
+import ListItem from '@mui/joy/ListItem';
+import ListItemButton, { listItemButtonClasses } from '@mui/joy/ListItemButton';
+import ListItemContent from '@mui/joy/ListItemContent';
+import Sheet from '@mui/joy/Sheet';
+import Typography from '@mui/joy/Typography';
+import { Link } from 'react-router-dom';
+
+import ColorSchemeToggle from './UI/ColorSchemeToggle';
+import Toggler from './UI/Toggler';
+
+
+export default function Sidebar() {
+  return (
+    <Sheet
+      className="Sidebar"
+      sx={{
+        position: { xs: 'fixed', md: 'sticky' },
+        transform: {
+          xs: 'translateX(calc(100% * (var(--SideNavigation-slideIn, 0) - 1)))',
+          md: 'none',
+        },
+        transition: 'transform 0.4s, width 0.4s',
+        zIndex: 10000,
+        height: '100dvh',
+        width: 'var(--Sidebar-width)',
+        top: 0,
+        p: 2,
+        flexShrink: 0,
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 2,
+        borderRight: '1px solid',
+        borderColor: 'divider',
+      }}
+    >
+      <GlobalStyles
+        styles={(theme) => ({
+          ':root': {
+            '--Sidebar-width': '220px',
+            [theme.breakpoints.up('lg')]: {
+              '--Sidebar-width': '240px',
+            },
+          },
+        })}
+      />
+      <Box
+        className="Sidebar-overlay"
+        sx={{
+          position: 'fixed',
+          zIndex: 9998,
+          top: 0,
+          left: 0,
+          width: '100vw',
+          height: '100vh',
+          opacity: 'var(--SideNavigation-slideIn)',
+          backgroundColor: 'var(--joy-palette-background-backdrop)',
+          transition: 'opacity 0.4s',
+          transform: {
+            xs: 'translateX(calc(100% * (var(--SideNavigation-slideIn, 0) - 1) + var(--SideNavigation-slideIn, 0) * var(--Sidebar-width, 0px)))',
+            lg: 'translateX(-100%)',
+          },
+        }}
+      />
+      <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
+        <Typography level="title-lg">🍅 西红柿读书</Typography>
+        <ColorSchemeToggle sx={{ ml: 'auto' }} />
+      </Box>
+      <Input size="sm" startDecorator={<SearchRoundedIcon />} placeholder="Search" />
+      <Box
+        sx={{
+          minHeight: 0,
+          overflow: 'hidden auto',
+          flexGrow: 1,
+          display: 'flex',
+          flexDirection: 'column',
+          [`& .${listItemButtonClasses.root}`]: {
+            gap: 1.5,
+          },
+        }}
+      >
+        <List
+          size="sm"
+          sx={{
+            gap: 1,
+            '--List-nestedInsetStart': '30px',
+            '--ListItem-radius': (theme) => theme.vars.radius.sm,
+          }}
+        >
+          <ListItem>
+            <ListItemButton>
+              <HomeRoundedIcon />
+              <ListItemContent>
+                <Link to="/">
+                  <Typography level="title-sm">首页</Typography>
+                </Link>
+              </ListItemContent>
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem nested>
+            <Toggler
+              defaultExpanded
+              renderToggle={({ open, setOpen }) => (
+                <ListItemButton onClick={() => setOpen(!open)}><ShoppingCartRoundedIcon />
+                  <ListItemContent>
+                    <Typography level="title-sm">订单</Typography>
+                  </ListItemContent>
+                  <KeyboardArrowDownIcon
+                    sx={[
+                      open ? { transform: 'rotate(180deg)' } : { transform: 'none' },
+                    ]}
+                  />
+                </ListItemButton>
+              )}
+            >
+              <List sx={{ gap: 0.5 }}>
+                <ListItem sx={{ mt: 0.5 }}><ListItemButton>待支付</ListItemButton></ListItem>
+                <ListItem><ListItemButton>待收货</ListItemButton></ListItem>
+              </List>
+            </Toggler>
+          </ListItem>
+        </List>
+        <List
+          size="sm"
+          sx={{
+            mt: 'auto',
+            flexGrow: 0,
+            '--ListItem-radius': (theme) => theme.vars.radius.sm,
+            '--List-gap': '8px',
+            mb: 2,
+          }}
+        >
+          <ListItem>
+            <ListItemButton
+              role="menuitem"
+            >
+              <AccountCircleIcon />
+              <ListItemContent>
+                <Link to="/profile">
+                  <Typography level="title-sm">个人中心</Typography>
+                </Link>
+              </ListItemContent>
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Box>
+    </Sheet>
+  );
+}
