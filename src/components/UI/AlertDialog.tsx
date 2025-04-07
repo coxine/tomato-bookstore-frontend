29ff4800c5aed153c6b55c0fc2de7b73bc71c710
栖鸟闲榆 <59347549+coxine@users.noreply.github.com>
2025-04-07 09:07:39 +0800

    feat: add AlertDialog component and utility function for displaying alerts

---
+++ src/components/UI/AlertDialog.tsx
@@ -0,0 +1,44 @@
+import WarningRoundedIcon from '@mui/icons-material/WarningRounded';
+import Button from '@mui/joy/Button';
+import DialogActions from '@mui/joy/DialogActions';
+import DialogContent from '@mui/joy/DialogContent';
+import DialogTitle from '@mui/joy/DialogTitle';
+import Divider from '@mui/joy/Divider';
+import Modal from '@mui/joy/Modal';
+import ModalDialog from '@mui/joy/ModalDialog';
+import * as React from 'react';
+
+export interface AlertDialogModalProps {
+  title: string;
+  description: string;
+  actions: React.ReactNode;
+  open: boolean;
+  onClose: () => void;
+}
+
+export default function AlertDialogModal({
+  title,
+  description,
+  actions,
+  open,
+  onClose,
+}: AlertDialogModalProps) {
+  return (
+    <Modal open={open} onClose={onClose}>
+      <ModalDialog variant="outlined" role="alertdialog">
+        <DialogTitle>
+          <WarningRoundedIcon />
+          {title}
+        </DialogTitle>
+        <Divider />
+        <DialogContent>{description}</DialogContent>
+        <DialogActions>
+          {actions}
+          <Button variant="outlined" color="neutral" onClick={onClose}>
+            取消
+          </Button>
+        </DialogActions>
+      </ModalDialog>
+    </Modal>
+  );
+}
---
+++ src/components/UI/AlertDialogUtils.tsx
@@ -0,0 +1,28 @@
+import { createRoot } from "react-dom/client"
+
+import AlertDialogModal from "./AlertDialog"
+
+export default function showAlertDialog(
+  title: string,
+  description: string,
+  actions: (close: () => void) => React.ReactNode, // 改为接收一个函数
+) {
+  const container = document.createElement('div')
+  document.body.appendChild(container)
+  const root = createRoot(container)
+
+  const handleClose = () => {
+    root.unmount()
+    container.remove()
+  }
+
+  root.render(
+    <AlertDialogModal
+      title={title}
+      description={description}
+      actions={actions(handleClose)}  // 将 close 回调传入 actions
+      open={true}
+      onClose={handleClose}
+    />
+  )
+}
