6dec46af8bdfe8df3b0c7e44e18fb52a4e867fe6
栖鸟闲榆 <59347549+coxine@users.noreply.github.com>
2025-04-22 18:43:06 +0800

    feat: update Loading component to support dark mode

--- src/components/UI/Loading.tsx
+++ src/components/UI/Loading.tsx
@@ -3,7 +3,7 @@ import { Box, CircularProgress } from '@mui/joy'
 export default function Loading() {
   return (
     <Box
-      sx={{
+      sx={(theme) => ({
         position: 'fixed',
         top: 0,
         left: 0,
@@ -13,8 +13,11 @@ export default function Loading() {
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: 'rgba(255, 255, 255, 0.7)',
+        [theme.getColorSchemeSelector('dark')]: {
+          backgroundColor: 'rgba(19 19 24 / 0.6)',
+        },
         zIndex: 9999,
-      }}
+      })}
     >
       <CircularProgress />
     </Box>
