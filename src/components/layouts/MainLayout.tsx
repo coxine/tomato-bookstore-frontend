e9c6ca5ca97528bed9d7e5141c2112c26ac93077
栖鸟闲榆 <59347549+coxine@users.noreply.github.com>
2025-05-09 23:38:40 +0800

    fix: remove sticky positioning from MainLayout component

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -40,7 +40,6 @@ export default function MainLayout({
         <Box sx={{ flex: 1, width: '100%' }}>
           <Box
             sx={{
-              position: 'sticky',
               top: { sm: -100, md: -110 },
               bgcolor: 'background.body',
               zIndex: 1,
